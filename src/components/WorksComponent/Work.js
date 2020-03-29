741bedca1497e6938048ca6c37e90bf656a31492
Marcin MALAK <marcin.malak.ext@alten-technology.com>
2020-03-29 20:19:45 +0200

    work section changed; add slim line decorator

--- src/components/WorksComponent/Work.js
+++ src/components/WorksComponent/Work.js
@@ -8,60 +8,53 @@ import project4 from '../../assets/project4.png';
 export default function Work() {
     return (
         <div className='works'>
-            <span className='main-label-name'>
-                    Selected work
-                </span>
-            <div className='project-container'>
-                <a className='label'>
-                    <span className='label-heading'>
-                        <span>Project 1</span><br />
-                        <span>description</span><br />
-                        <span>here!</span><br />
-                    </span>
-                    <span className='label-data'>
-                        January 2019
-                    </span>
-                </a>
-                <img id='name-of-project' src={project1}></img>
+            <div className='main-label-name'>
+                    <h1>Selected</h1>
+                    <h1>work</h1>
             </div>
-            <div className='project-container'>
+            <div className='project-container'> 
+            <img id='name-of-project' src={project1}></img>
                 <a className='label'>
-                    <span className='label-heading'>
-                        <span>Project 2</span><br />
-                        <span>description</span><br />
-                        <span>here!</span><br />
-                    </span>
                     <span className='label-data'>
-                        June 2019
+                        January 2019 <br />
+                    </span>
+                    <span className='label-heading'>
+                        <span>project name</span>
                     </span>
                 </a>
-                <img id='name-of-project' src={project2}></img>
             </div>
-            <div className='project-container'>
+            <div className='project-container'> 
+            <img id='name-of-project' src={project2}></img>
                 <a className='label'>
-                    <span className='label-heading'>
-                        <span>Project 3</span><br />
-                        <span>description</span><br />
-                        <span>here!</span><br />
-                    </span>
                     <span className='label-data'>
-                        November 2019
+                        January 2019 <br />
+                    </span>
+                    <span className='label-heading'>
+                        <span>project name</span>
                     </span>
                 </a>
-                <img id='name-of-project' src={project3}></img>
             </div>
-            <div className='project-container'>
+            <div className='project-container'> 
+            <img id='name-of-project' src={project3}></img>
                 <a className='label'>
-                    <span className='label-heading'>
-                        <span>Project 4</span><br />
-                        <span>description</span><br />
-                        <span>here!</span><br />
-                    </span>
                     <span className='label-data'>
-                        February 2020
+                        January 2019 <br />
+                    </span>
+                    <span className='label-heading'>
+                        <span>project name</span>
                     </span>
                 </a>
-                <img id='name-of-project' src={project4}></img>
+            </div>
+            <div className='project-container'> 
+            <img id='name-of-project' src={project4}></img>
+                <a className='label'>
+                    <span className='label-data'>
+                        January 2019 <br />
+                    </span>
+                    <span className='label-heading'>
+                        <span>project name</span>
+                    </span>
+                </a>
             </div>
         </div>
     )
