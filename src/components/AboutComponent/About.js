0689a7f3101a552047eae4a8714f9ccf6e8ebfd1
Marcin MALAK <marcin.malak.ext@alten-technology.com>
2020-04-11 12:56:05 +0200

    React-inversection-observer added

--- src/components/AboutComponent/About.js
+++ src/components/AboutComponent/About.js
@@ -1,23 +1,66 @@
 import React from 'react'
+import { useInView } from 'react-intersection-observer';
 import '../AboutComponent/About.scss'
 
+
 export default function About() {
+
+    const [ref, inView] = useInView({
+        root: null,
+        threshold: 0.3,
+        triggerOnce: true,
+    })
+
+    const [ref2, inView2] = useInView({
+        root: null,
+        threshold: 0.3,
+        triggerOnce: true,
+    })
+
     return (
+
         <div className='about'>
             <div className='bio-text-container'>
                 <div className='bio-element-wrapper'>
-                    <h1 className='bio-element'>
-                        About me
-                    </h1>
-                    <h2 className='bio-element'>
-                        Have a look!
-                    </h2>
-                    <p className='bio-element-description'>
-                    Freshly graduated student from <a href="" title="Wroclaw University of Science and Technology" target="_blank">Wroclaw University of Science and Technology</a>, partly made in <a href="https://hetic.net" title="Universitat Autonoma de Barcelona" target="_blank">Universitat Autonoma de Barcelona</a>. 
-                    Last years I had the chance to did an internship at <a href="" title="Explain Everything" target="_blank">Explain Everything </a> 
-                    (2018/2019), as Android developer, after that I was working for my university as data scientist (2018). During my master thesis I was cooparating with Ateknea company (2019) to create my master thesis "Forecasting level of air pollution in urban areas".
-                    <br /> <br />Currently (2020) working for Airbus in Hamburg. Enjoying a great love of aesthetic graphics and projects. Still improving the knowledge of web development! <br /> <br /> 
-                    Open for any <a href="" title="mmalak portfolio" target="_blank"> collaporation! </a>
+                    <div ref={ref} className='bio-element'>
+                        <h1>
+                            {inView ? (
+                                <span className="a-1-h1">About me</span>
+                            ) : (
+                                    <span className="a1">About me</span>
+                                )}
+                        </h1>
+                        <h2>
+                            {inView ? (
+                                <span className="a-2-h1">Have a look!</span>
+                            ) : (
+                                    <span className="a2">work</span>
+                                )}
+                        </h2>
+                    </div>
+                    <p ref={ref2} className='bio-element-description'>
+                        <div className='a-2-d'>
+                            {inView2 ? (
+                                <span className="a-2-span">
+                                    Freshly graduated student from <a href="" title="Wroclaw University of Science and Technology" target="_blank">Wroclaw University of Science and Technology</a>, partly made in <a href="https://hetic.net" title="Universitat Autonoma de Barcelona" target="_blank">Universitat Autonoma de Barcelona</a>.
+                        Last years I had the chance to did an internship at <a href="" title="Explain Everything" target="_blank">Explain Everything </a>
+                        (2018/2019), as Android developer, after that I was working for my university as data scientist (2018). During my master thesis I was cooparating with Ateknea company (2019) to create my master thesis "Forecasting level of air pollution in urban areas".
+                                    <br /> <br />Currently (2020) working for Airbus in Hamburg. Enjoying a great love of aesthetic graphics and projects. Still improving the knowledge of web development! <br /> <br />
+                        Open for any <a href="" title="mmalak portfolio" target="_blank"> collaporation! </a>
+                                </span>
+                            )
+                                : (
+                                    <span span className="a-2">
+                                        Freshly graduated student from <a href="" title="Wroclaw University of Science and Technology" target="_blank">Wroclaw University of Science and Technology</a>, partly made in <a href="https://hetic.net" title="Universitat Autonoma de Barcelona" target="_blank">Universitat Autonoma de Barcelona</a>.
+                        Last years I had the chance to did an internship at <a href="" title="Explain Everything" target="_blank">Explain Everything </a>
+                        (2018/2019), as Android developer, after that I was working for my university as data scientist (2018). During my master thesis I was cooparating with Ateknea company (2019) to create my master thesis "Forecasting level of air pollution in urban areas".
+                                        <br /> <br />Currently (2020) working for Airbus in Hamburg. Enjoying a great love of aesthetic graphics and projects. Still improving the knowledge of web development! <br /> <br />
+                        Open for any <a href="" title="mmalak portfolio" target="_blank"> collaporation! </a>
+                                    </span>
+                                )}
+                        </div>
+
+
                     </p>
                 </div>
             </div>
--- src/components/WorksComponent/Work.js
+++ src/components/WorksComponent/Work.js
@@ -11,8 +11,33 @@ import project4 from '../../assets/project4.png';
 export default function Work() {
 
     const [ref, inView] = useInView({
-        threshold: 1,
-        // triggerOnce: true,
+        root: null,
+        threshold: 0.3,
+        triggerOnce: true,
+    })
+
+    const [ref2, inView2] = useInView({
+        root: null,
+        threshold: 0.6,
+        triggerOnce: true,
+    })
+
+    const [ref3, inView3] = useInView({
+        root: null,
+        threshold: 0.3,
+        triggerOnce: true,
+    })
+
+    const [ref4, inView4] = useInView({
+        root: null,
+        threshold: 0.3,
+        triggerOnce: true,
+    })
+
+    const [ref5, inView5] = useInView({
+        root: null,
+        threshold: 0.3,
+        triggerOnce: true,
     })
 
     return (
@@ -33,18 +58,17 @@ export default function Work() {
                         )}
                 </h1>
             </div>
-
-            <div ref={ref} className='project-container'>
+            <div ref={ref2} className='project-container'>
                 <div className='img-wrap'>
                     <img id='project-img' src={project1}></img>
-                    {inView ? (
+                    {inView2 ? (
                         <span className='w-1-img-cover'></span>
                     ) : (
                             <span className='img-cover'></span>
                         )}
                 </div>
-                <a ref={ref} className='label'>
-                    {inView ? (
+                <a className='label'>
+                    {inView2 ? (
                         <span>
                             <span className='label-data-l1'>
                                 January 2019 <br />
@@ -58,21 +82,183 @@ export default function Work() {
                             </span>
 
                         )}
-                    {inView ? (
+                    {inView2 ? (
                         <span>
                             <span className='label-heading-l1'>
                                 project name
                             </span>
                         </span>
                     ) : (
-                        <span>
-                            <span className='label-heading'>
-                                <span>project name</span>
+                            <span>
+                                <span className='label-heading'>
+                                    <span>project name</span>
+                                </span>
                             </span>
-                        </span>
                         )}
                 </a>
+                {inView2 ? (
+                    <span>
+                        <span className='label-line-l1'>
+                        </span>
+                    </span>
+                ) : (
+                        <span>
+                            <span className='label-line'>
+                            </span>
+                        </span>
+                    )}
+            </div>
+            <div ref={ref3} className='project-container'>
+                <div className='img-wrap'>
+                    <img id='project-img' src={project2}></img>
+                    {inView3 ? (
+                        <span className='w-1-img-cover'></span>
+                    ) : (
+                            <span className='img-cover'></span>
+                        )}
+                </div>
+                <a className='label'>
+                    {inView3 ? (
+                        <span>
+                            <span className='label-data-l1'>
+                                January 2019 <br />
+                            </span>
+                        </span>
+                    ) : (
+                            <span>
+                                <span className='label-data'>
+                                    January 2019 <br />
+                                </span>
+                            </span>
+
+                        )}
+                    {inView3 ? (
+                        <span>
+                            <span className='label-heading-l1'>
+                                project name 2
+                            </span>
+                        </span>
+                    ) : (
+                            <span>
+                                <span className='label-heading'>
+                                    <span>project name 2</span>
+                                </span>
+                            </span>
+                        )}
+                </a>
+                {inView3 ? (
+                    <span>
+                        <span className='label-line-l1'>
+                        </span>
+                    </span>
+                ) : (
+                        <span>
+                            <span className='label-line'>
+                            </span>
+                        </span>
+                    )}
+            </div>
+            <div ref={ref4} className='project-container'>
+                <div className='img-wrap'>
+                    <img id='project-img' src={project3}></img>
+                    {inView4 ? (
+                        <span className='w-1-img-cover'></span>
+                    ) : (
+                            <span className='img-cover'></span>
+                        )}
+                </div>
+                <a className='label'>
+                    {inView4 ? (
+                        <span>
+                            <span className='label-data-l1'>
+                                January 2019 <br />
+                            </span>
+                        </span>
+                    ) : (
+                            <span>
+                                <span className='label-data'>
+                                    January 2019 <br />
+                                </span>
+                            </span>
+
+                        )}
+                    {inView4 ? (
+                        <span>
+                            <span className='label-heading-l1'>
+                                project name 3
+                            </span>
+                        </span>
+                    ) : (
+                            <span>
+                                <span className='label-heading'>
+                                    <span>project name 3</span>
+                                </span>
+                            </span>
+                        )}
+                </a>
+                {inView4 ? (
+                    <span>
+                        <span className='label-line-l1'>
+                        </span>
+                    </span>
+                ) : (
+                        <span>
+                            <span className='label-line'>
+                            </span>
+                        </span>
+                    )}
+            </div>
+            <div ref={ref5} className='project-container'>
+                <div className='img-wrap'>
+                    <img id='project-img' src={project4}></img>
+                    {inView5 ? (
+                        <span className='w-1-img-cover'></span>
+                    ) : (
+                            <span className='img-cover'></span>
+                        )}
+                </div>
+                <a className='label'>
+                    {inView5 ? (
+                        <span>
+                            <span className='label-data-l1'>
+                                January 2019 <br />
+                            </span>
+                        </span>
+                    ) : (
+                            <span>
+                                <span className='label-data'>
+                                    January 2019 <br />
+                                </span>
+                            </span>
+
+                        )}
+                    {inView5 ? (
+                        <span>
+                            <span className='label-heading-l1'>
+                                project name 4
+                            </span>
+                        </span>
+                    ) : (
+                            <span>
+                                <span className='label-heading'>
+                                    <span>project name 4</span>
+                                </span>
+                            </span>
+                        )}
+                </a>
+                {inView5 ? (
+                    <span>
+                        <span className='label-line-l1'>
+                        </span>
+                    </span>
+                ) : (
+                        <span>
+                            <span className='label-line'>
+                            </span>
+                        </span>
+                    )}
             </div>
         </div>
+
     )
 }
