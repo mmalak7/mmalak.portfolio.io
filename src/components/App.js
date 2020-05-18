2ac13d370f1b0f1b49eb964160851f8a2a5e653d
Marcin MALAK <marcin.malak.ext@alten-technology.com>
2020-05-18 19:15:28 +0200

    style about components for high resolution

--- src/components/App.js
+++ src/components/App.js
@@ -18,8 +18,8 @@ TODO: refactor scss variables and classes ;) - DONE!
 */
 
 const routes = [
-    { path: "/", name: 'Home', Component: HomeHighResolution },
-    { path: "/about", name: 'About', Component: AboutHighResolution },
+    // { path: "/", name: 'Home', Component: HomeHighResolution },
+    { path: "/", name: 'About', Component: AboutHighResolution },
     { path: "/works", name: 'works', Component: WorkHighResolution },
     { path: "/contact", name: 'Contact', Component: ContactHighResolution },
 ]
