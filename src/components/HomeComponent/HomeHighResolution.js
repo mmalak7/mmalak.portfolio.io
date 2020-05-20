4695c855a0656d79aaaa2483ebca97de0ab2ec07
Marcin MALAK <marcin.malak.ext@alten-technology.com>
2020-05-20 08:52:17 +0200

    home txt anim added

--- src/components/HomeComponent/HomeHighResolution.js
+++ src/components/HomeComponent/HomeHighResolution.js
@@ -1,17 +1,53 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Pavilon from '../../assets/pavillon-entrance.jpg'
+import '../../stylesheets/layout/laptop/HomeHighRes.scss';
+
+import CSSRulePlugin from "gsap/CSSRulePlugin";
+import gsap from 'gsap';
 
 const HomeHighResolution = () => {
+
+    let pavilonPhoto = useRef(null);
+    let quote = useRef(null);
+
+    let line1 = useRef(null);
+    let line1After = CSSRulePlugin.getRule(".h1-1:after");
+    // let line2 = useRef(null);
+    // let line2After = CSSRulePlugin.getRule("h1-2:after");
+    let line3 = useRef(null);
+    let line3After = CSSRulePlugin.getRule(".h4-1:after");
+
+    useEffect(() => {
+
+        gsap.to(quote, {
+            visibility: "visible"
+        });
+
+        gsap.to([line1After], {
+            duration: .8,
+            width: "0%",
+            ease: "power3.out",
+        });
+
+        gsap.to(line3After, {
+            duration: 0.8,
+            width: "0%",
+            ease: "power3.out",
+        });
+    }, [])
+
     return (
         <div className="home-page">
             <div className="container">
                 <div className='pavilon-photo'>
-                    <img src={Pavilon} alt="" />
+                    <img ref={el => (pavilonPhoto = el)} src={Pavilon} alt="" />
                 </div>
-                <div className="quote-home">
-                    <h1>Less is</h1>
-                    <h1>more</h1>
-                    <h4>~Ludwig Mies van der Rohe</h4>
+                <div ref={el => quote = el} className="quote-home">
+                    <div className='mies'>
+                        <h1 className='h1-1' >Less is<br/> more</h1>
+                        {/* <h1 className='h1-1'>more</h1> */}
+                    </div>
+                    <h4 ref={el => (line3 = el)} className='h4-1'>~Ludwig Mies van der Rohe</h4>
                 </div>
             </div>
         </div>
