4b3f2b32d106fcac5215763ba23bf45543aeed29
Marcin MALAK <marcin.malak.ext@alten-technology.com>
2020-05-20 10:01:24 +0200

    about anim added

--- src/components/AboutComponent/AboutHighResolution.js
+++ src/components/AboutComponent/AboutHighResolution.js
@@ -1,24 +1,67 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Pavilon from '../../assets/pavillon-entrance.jpg'
+import gsap from 'gsap';
 
 const AboutHighResolution = () => {
+
+    let pavilonPhotoAbout = useRef(null);
+    let bioDescription = useRef(null);
+    let bio = useRef(null);
+
+    let one = useRef(null);
+    let two = useRef(null);
+    let three = useRef(null);
+    let four = useRef(null);
+    let five = useRef(null);
+    let six = useRef(null);
+
+    useEffect(() => {
+        gsap.from(pavilonPhotoAbout, {
+            duration: 2,
+            opacity: 0,
+            y: 30,
+            ease: "power3.out",
+        })
+
+        gsap.to(bioDescription, {
+            visibility: "visible"
+        });
+
+        gsap.to([one, two, three, four, five, six], {
+            delay: 1.4,
+            duration: 1.8,
+            opacity: 1,
+            ease: "power3.out",
+            stagger: {
+                amount: .5,
+            }
+        })
+    }, [pavilonPhotoAbout])
+
+
     return (
         <div className="about-page">
             <div className="container">
-                <p className='bio-element-description'>
-                    <span className="a-2-span">
+                <p ref={el => bioDescription = el} className='bio-element-description'>
+                    {/* <span ref={el => bio = el} className="a-2-span">
                         Freshly graduated student from <a href="http://pwr.edu.pl/en/" title="Wroclaw University of Science and Technology" target="_blank">Wroclaw University of Science and Technology</a>, partly made in <a href="https://www.uab.cat/en/" title="Universitat Autonoma de Barcelona" target="_blank">Universitat Autonoma de Barcelona</a>.
                         Last years I had the chance to did an internship at <a href="https://explaineverything.com/" title="Explain Everything" target="_blank">Explain Everything </a>
                         (2018/2019), as Android developer, after that I was working for my university as data scientist (2018). During my master thesis I was cooparating with Ateknea company (2019) to create my master thesis "Forecasting level of air pollution in urban areas".
                                     <br /> <br />Currently (2020) working for Airbus in Hamburg. Enjoying a great love of aesthetic graphics and projects. Still improving the knowledge of web development! <br /> <br />
                         Open for any <a href="" title="mmalak portfolio" target="_blank"> collaporation! </a>
-                    </span>
+                    </span> */}
+                    <p ref={el => one = el} className="a-2-span">Freshly graduated student from <a href="http://pwr.edu.pl/en/" title="Wroclaw University of Science and Technology" target="_blank">Wroclaw University of Science and Technology</a>,</p>
+                    <p ref={el => two = el} className="a-2-span">partly made in <a href="https://www.uab.cat/en/" title="Universitat Autonoma de Barcelona" target="_blank">Universitat Autonoma de Barcelona</a>.</p>
+                    <p ref={el => three = el} className="a-2-span">Last years I had the chance to did an internship at <a href="https://explaineverything.com/" title="Explain Everything" target="_blank">Explain Everything </a>                   </p>
+                    <p ref={el => four = el} className="a-2-span">(2018/2019), as Android developer, after that I was working for my university as data scientist (2018). During my master thesis I was cooparating with Ateknea company (2019) to create my master thesis "Forecasting level of air pollution in urban areas".</p>
+                    <p ref={el => five = el} className="a-2-span"><br /> <br />Currently (2020) working for Airbus in Hamburg. Enjoying a great love of aesthetic graphics and projects. Still improving the knowledge of web development! <br /> <br /></p>
+                    <p ref={el => six = el} className="a-2-span">Open for any <a href="" title="mmalak portfolio" target="_blank"> collaporation! </a></p>
                 </p>
                 <div className='owner-photo'>
-                    <img src={Pavilon} alt="" />
+                    <img ref={el => pavilonPhotoAbout = el} src={Pavilon} alt="" />
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
--- src/components/HomeComponent/HomeHighResolution.js
+++ src/components/HomeComponent/HomeHighResolution.js
@@ -10,31 +10,33 @@ const HomeHighResolution = () => {
     let pavilonPhoto = useRef(null);
     let quote = useRef(null);
 
-    let line1 = useRef(null);
     let line1After = CSSRulePlugin.getRule(".h1-1:after");
-    // let line2 = useRef(null);
-    // let line2After = CSSRulePlugin.getRule("h1-2:after");
-    let line3 = useRef(null);
     let line3After = CSSRulePlugin.getRule(".h4-1:after");
 
     useEffect(() => {
+
+
+        gsap.from(pavilonPhoto, {
+            duration: 1.4,
+            opacity: 0,
+            y: 30,
+            ease: "power3.out",
+        })
 
         gsap.to(quote, {
             visibility: "visible"
         });
 
-        gsap.to([line1After], {
-            duration: .8,
+        gsap.to([line1After, line3After], {
+            delay: 1.4,
+            duration: 1.8,
             width: "0%",
             ease: "power3.out",
-        });
-
-        gsap.to(line3After, {
-            duration: 0.8,
-            width: "0%",
-            ease: "power3.out",
-        });
-    }, [])
+            stagger: {
+                amount: 1
+            }
+        })
+    }, [pavilonPhoto])
 
     return (
         <div className="home-page">
@@ -44,10 +46,9 @@ const HomeHighResolution = () => {
                 </div>
                 <div ref={el => quote = el} className="quote-home">
                     <div className='mies'>
-                        <h1 className='h1-1' >Less is<br/> more</h1>
-                        {/* <h1 className='h1-1'>more</h1> */}
+                        <h1 className='h1-1' >Less is<br /> more</h1>
                     </div>
-                    <h4 ref={el => (line3 = el)} className='h4-1'>~Ludwig Mies van der Rohe</h4>
+                    <h4 className='h4-1'>~Ludwig Mies van der Rohe</h4>
                 </div>
             </div>
         </div>
