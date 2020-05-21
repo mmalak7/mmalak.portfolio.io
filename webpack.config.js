849a694df5775bec580ef5cea6301bda3ff318ac
Marcin MALAK <marcin.malak.ext@alten-technology.com>
2020-05-21 12:04:30 +0200

    update package.json

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/index.js',
+    // devtool: 'inline-source-map',
     output: {
         path: path.join(__dirname, '/dist'),
         filename: 'index_bundle.js'
